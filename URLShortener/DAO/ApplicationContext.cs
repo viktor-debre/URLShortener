56617c2a11f0562bda3013ef9d772ffe6ef656da
viktor-debre <viktor.debre2002@gmail.com>
2023-01-04 21:15:30 +0200

    Add context for database connection

---
+++ URLShortener/DAO/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortener.Models;
+
+namespace URLShortener.DAO
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        //Entities
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<URL> URLs { get; set; }
+    }
+}
