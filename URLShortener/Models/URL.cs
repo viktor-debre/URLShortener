6942f4d988e654c1c854426bde67ac937048a8a4
viktor-debre <viktor.debre2002@gmail.com>
2023-01-04 21:16:00 +0200

    Add models

---
+++ URLShortener/Models/URL.cs
@@ -0,0 +1,15 @@
+﻿namespace URLShortener.Models
+{
+    public class URL
+    {
+        public int Id { get; set; }
+
+        public string FullUrl { get; set; }
+
+        public string? ShortUrl { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedDate { get; set;}
+    }
+}
---
+++ URLShortener/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace URLShortener.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+    }
+}
