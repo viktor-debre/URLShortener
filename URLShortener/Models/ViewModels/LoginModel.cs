a6388d48c0ee119904b7495e54414a1ebb2daed7
viktor-debre <viktor.debre2002@gmail.com>
2023-01-05 15:53:21 +0200

    Add view models and update views for them

---
+++ URLShortener/Models/ViewModels/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortener.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Not specified login")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Not specified password")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Password must be more than 4 characters")]
+        public string Password { get; set; }
+    }
+}
---
+++ URLShortener/Models/ViewModels/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace URLShortener.Models.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Not specified email")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage = "Not specified password")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Password must be more than 4 characters")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
