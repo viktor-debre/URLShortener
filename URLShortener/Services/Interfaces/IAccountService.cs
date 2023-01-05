ec5d2c7c3770c94ec451a04dbef95361d5adf33c
viktor-debre <viktor.debre2002@gmail.com>
2023-01-05 15:54:18 +0200

    Add service for authorization

---
+++ URLShortener/Services/Interfaces/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using URLShortener.Models.Entities;
+
+namespace URLShortener.Services.Interfaces
+{
+    public interface IAccountService
+    {
+        public Task<User> AuthenticateUser(string login, string password);
+
+        public Task<bool> RegisterUser(string login, string password);
+    }
+}
---
+++ URLShortener/Services/Realization/AccountService.cs
@@ -0,0 +1,59 @@
+﻿using URLShortener.DAO.Interfaces;
+using URLShortener.Models;
+using URLShortener.Models.Entities;
+using URLShortener.Services.Interfaces;
+
+namespace URLShortener.Services.Realization
+{
+    public class AccountService : IAccountService
+    {
+        public IUnitOfWork _unitOfWork;
+
+        public AccountService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<User> AuthenticateUser(string login, string password)
+        {
+            var usersList = await _unitOfWork.Users.GetAll();
+            var user = usersList.FirstOrDefault(user => user.Login == login && user.Password == password);
+
+            return user;
+        }
+
+        public async Task<bool> RegisterUser(string login, string password)
+        {
+            var usersList = await _unitOfWork.Users.GetAll();
+            var user = usersList.FirstOrDefault(user => user.Login == login && user.Password == password);
+            int id;
+
+            if(usersList.Count() != 0)
+            {
+                id = usersList.Last().Id + 1;
+            }
+            else
+            {
+                id = 1;
+            }
+
+            if (user == null)
+            {
+                
+                await _unitOfWork.Users.Add(new User {Id = id, Login = login, Password = password, Role = "user" });
+
+                var result = _unitOfWork.Save();
+
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
